180ff0104c7604962736edecbca29e76fc3ba61b
M Namitha Pawar <mnamitha.pawar@hitachids.com>
2026-01-30 14:10:13 +0530

    Use Case 2: Environment-based configuration

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask
+import os
+
+print("🔥 NEW CODE IS RUNNING 🔥")
+print("APP_ENV =", os.getenv("APP_ENV"))
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Application is running"
+    env = os.getenv("APP_ENV")
+    return f"Application is running in {env} environment"
 
 @app.route("/health")
 def health():
     return "OK"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
+
