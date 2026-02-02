4fbe4f5912005e2c8ceb6096e8453efc4e5c7665
M Namitha Pawar <mnamitha.pawar@hitachids.com>
2026-02-02 12:18:38 +0530

    Fixed flake8 formatting issues

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask
 import os
 
-print("🔥 NEW CODE IS RUNNING 🔥")
-print("APP_ENV =", os.getenv("APP_ENV"))
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    env = os.getenv("APP_ENV")
+    env = os.getenv("APP_ENV", "DEV")
     return f"Application is running in {env} environment"
+
 
 @app.route("/health")
 def health():
     return "OK"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
